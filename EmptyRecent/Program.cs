5d6b114495521dbcd1c38185697e625b22b60063
Weespin <wnmerukini@gmail.com>
2017-11-15 20:35:14 +0200

    First Commit

--- EmptyRecent/Program.cs
+++ EmptyRecent/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
+using Steamworks;
 
 namespace EmptyRecent
 {
@@ -10,6 +11,33 @@ namespace EmptyRecent
     {
         static void Main(string[] args)
         {
+            if (SteamAPI.IsSteamRunning())
+            {
+                
+                Console.WriteLine("EmptyRecent tool by Weespin");
+                Console.WriteLine("Running fake game #1");
+                Environment.SetEnvironmentVariable("SteamAppId", "399220");
+                SteamAPI.Init();
+                SteamAPI.Shutdown();
+                Console.WriteLine("Stopping playing fake game #1");
+                Console.WriteLine("Running fake game #2");
+                Environment.SetEnvironmentVariable("SteamAppId", "399510");
+                SteamAPI.Init();
+                SteamAPI.Shutdown();
+                Console.WriteLine("Stopping playing fake game #2");
+                Console.WriteLine("Running fake game #3");
+                Environment.SetEnvironmentVariable("SteamAppId", "399480");
+                SteamAPI.Init();
+                SteamAPI.Shutdown();
+                Console.WriteLine("Stopping playing fake game #3");
+                Console.WriteLine("Done!");
+            }
+            else
+            {
+               Console.WriteLine("Steam is not running!");
+            }
+            Console.WriteLine("PRESS ANY KEY TO EXIT");
+            Console.ReadKey();
         }
     }
 }
--- EmptyRecent/Properties/AssemblyInfo.cs
+++ EmptyRecent/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EmptyRecent")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A small tool")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Weespin")]
 [assembly: AssemblyProduct("EmptyRecent")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
